4cee0e1a8c950614fa72a2ecdfca2e812625e96d
TOM-SKYNET <netmail.tom@gmail.com>
2019-10-22 19:59:50 +0100

    First commit

---
+++ src/test/java/org/tom/springboot/SpringBoot2JpaCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tom.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2JpaCurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
